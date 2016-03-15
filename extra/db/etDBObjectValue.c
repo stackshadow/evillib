756d8127d6ae50e02036aad55f83ab0d579a4d0e
stackshadow <stackshadow@evilbrain.de>
2016-03-15 17:46:06 +0100

    etDBObjectValue: Initial commit of handling DB values ( set/get from/to json )

---
+++ extra/db/etDBObjectValue.c
@@ -0,0 +1,108 @@
+/*  Copyright (C) 2014-2016 by Martin Langlotz alias stackshadow
+
+    This file is part of evillib.
+
+    evillib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    evillib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with evillib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "evillib_depends.h"
+#include "evillib-extra_depends.h"
+
+
+#include "core/etDebug.h"
+#include "core/etObject.h"
+#include "memory/etMemory.h"
+#include "db/etDBObject.h"
+#include "db/etDBObjectTable.h"
+#include "db/etDBObjectTableColumn.h"
+
+
+
+
+etID_STATE          etDBObjectValueClean( etDBObject *dbObject ){
+// check
+    etDebugCheckNull( dbObject );
+
+// vars
+    json_object_set_new( dbObject->jsonRootObject, "values", json_object() );
+
+    return etID_YES;
+}
+
+
+etID_STATE          etDBObjectValueSet( etDBObject *dbObject, const char *columnName, const char *value ){
+// check
+    etDebugCheckNull( dbObject );
+    etDebugCheckNull( columnName );
+    etDebugCheckNull( value );
+
+// vars
+    json_t          *jsonColumnValues = NULL;
+
+// get the object which hold all the values
+    jsonColumnValues = json_object_get( dbObject->jsonRootObject, "values" );
+    if( jsonColumnValues == NULL ){
+        jsonColumnValues = json_object();
+        json_object_set_new( dbObject->jsonRootObject, "values", jsonColumnValues );
+    }
+
+// save value
+    if( json_object_set_new( jsonColumnValues, columnName, json_string(value) ) != 0 ) return etID_STATE_ERR;
+    if( json_object_set_new( jsonColumnValues, "inDB", json_integer(0) ) != 0 ) return etID_STATE_ERR;
+
+
+
+    return etID_YES;
+}
+
+
+etID_STATE          __etDBObjectValueNext( etDBObject *dbObject, const char **p_columnName, const char **p_value ){
+// check
+    etDebugCheckNull( dbObject );
+    etDebugCheckNull( p_columnName );
+    etDebugCheckNull( p_value );
+
+
+
+// start iteration if needed
+    if( dbObject->jsonIterator == NULL ){
+
+    // vars
+        json_t          *jsonColumnValues = NULL;
+
+    // get the object which hold all the values
+        jsonColumnValues = json_object_get( dbObject->jsonRootObject, "values" );
+        if( jsonColumnValues == NULL ){
+            jsonColumnValues = json_object();
+            json_object_set_new( dbObject->jsonRootObject, "values", jsonColumnValues );
+        }
+
+    // start iteration
+        dbObject->jsonObjectToIterate = jsonColumnValues;
+        dbObject->jsonIterator = json_object_iter( dbObject->jsonObjectToIterate );
+
+    } else {
+    // iterate
+        dbObject->jsonIterator = json_object_iter_next( dbObject->jsonObjectToIterate, dbObject->jsonIterator );
+    }
+
+// column name
+    *p_columnName = json_object_iter_key( dbObject->jsonIterator );
+
+// column value
+    *p_value = json_string_value( json_object_iter_value(dbObject->jsonIterator) );
+
+    return etID_YES;
+}
+
